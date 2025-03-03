b0448dbec0ba70dd42c311db1c6f788244e4e27b
a.22308051281081 <a.22308051281081@cbtis128.edu.mx>
2025-03-03 18:16:23 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 146, 247, 213),
+        appBar: AppBar(
+          title: const Text(
+            "Luis Molinar MAT:1081",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 7, 204, 56), // Color de fondo del AppBar
+          centerTitle: true, // Centra el título en el AppBar
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.black, width: 3),
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(width: 20),
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(width: 20),
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://raw.githubusercontent.com/Luis-Molinar-1081/aeropuerto_imagenes_app_flutter/refs/heads/main/Buo.PNG'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
